694b7c0d8fdc69ca5656324a810d7cb3ad9862dc
Jasper Guo <jguo@seek.com.au>
2026-03-23 14:21:33 +1100

    update comment

--- src/App/ClickToRevealPureCSS/index.tsx
+++ src/App/ClickToRevealPureCSS/index.tsx
@@ -3,7 +3,7 @@ import { useRef } from 'react';
 
 import './index.css';
 
-// Frontend needs to detect the link, and 1. add class="masked-link" 2. add data-masked="masked text to show" to the <a> tag
+// Frontend needs to detect the link first, and then 1. add class="masked-link" 2. add data-masked="masked text to show" to the <a> tag
 const maskedHtml =
   '<p>job description</p><p></p><p></p><p></p><p><span>please call Steve Mifsud on </span><a class="masked-link" data-masked="(02) 97●● ●●94" target="_blank" rel="noopener noreferrer nofollow" href="tel:(02) 9765 4094"><strong><u>(02) 9765 4094</u></strong></a><span> or via&nbsp;</span><a target="_blank" rel="noopener noreferrer nofollow" class="masked-link" data-masked="st●●●●●●●●●●●●●●●●●●●●ov" href="mailto:steve.mifsud@dcj.nsw.gov.au"><strong><u>steve.mifsud@dcj.nsw.gov.au</u></strong></a></p><p></p>';
 
