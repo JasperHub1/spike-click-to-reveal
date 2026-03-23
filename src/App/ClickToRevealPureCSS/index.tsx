afc3932c04f3ee06dc45119e36eed75095692215
Jasper Guo <jguo@seek.com.au>
2026-03-23 14:26:19 +1100

    clean

--- src/App/ClickToRevealPureCSS/index.tsx
+++ src/App/ClickToRevealPureCSS/index.tsx
@@ -1,5 +1,4 @@
 import { UnsafeJobHtmlRenderer } from '@seek/job-html-renderer';
-import { useRef } from 'react';
 
 import './index.css';
 
@@ -7,14 +6,10 @@ import './index.css';
 const maskedHtml =
   '<p>job description</p><p></p><p></p><p></p><p><span>please call Steve Mifsud on </span><a class="masked-link" data-masked="(02) 97●● ●●94" target="_blank" rel="noopener noreferrer nofollow" href="tel:(02) 9765 4094"><strong><u>(02) 9765 4094</u></strong></a><span> or via&nbsp;</span><a target="_blank" rel="noopener noreferrer nofollow" class="masked-link" data-masked="st●●●●●●●●●●●●●●●●●●●●ov" href="mailto:steve.mifsud@dcj.nsw.gov.au"><strong><u>steve.mifsud@dcj.nsw.gov.au</u></strong></a></p><p></p>';
 
-export const ClickToRevealPureCSS = () => {
-  const containerRef = useRef<HTMLDivElement>(null);
-
+export const ClickToRevealPureCSS = () => (
   // Need extra hook to attach click handler for tracking purpose
 
-  return (
-    <div ref={containerRef} className="masked-link-container">
-      <UnsafeJobHtmlRenderer>{maskedHtml}</UnsafeJobHtmlRenderer>
-    </div>
-  );
-};
+  <div className="masked-link-container">
+    <UnsafeJobHtmlRenderer>{maskedHtml}</UnsafeJobHtmlRenderer>
+  </div>
+);
